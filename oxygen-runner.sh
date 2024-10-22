89c9b93dc5c358b932aa4ef1b9f4f9c5f97b98b0
Hunter Madsen <hmadsen@chicagoventure.com>
2024-10-22 10:56:29 -0500

    update

--- oxygen-runner.sh
+++ oxygen-runner.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
+BLUE='\033[0;34m'
+NC='\033[0m' # No Color
+
+# Check for the 'pim' flag
+if [ "$1" == "--pim" ]; then
+  echo -e "Setting PIM_BACKEND to ${BLUE}local${NC}"
+  export PIM_BACKEND="local"
+else
+    echo -e "Setting PIM_BACKEND to ${BLUE}production${NC}. Run with --pim to use a local PIM server."
+  export PIM_BACKEND="production"
+fi
+
+# Print the value of the PIM_BACKEND environment variable
+echo -e "PIM_BACKEND is set to ${BLUE}$PIM_BACKEND${NC}"
+
 # Change directory to the oxygen project directory
 echo "Changing directory to ~/Oxygen..."
 cd ~/Oxygen
--- typenex-medical-runner.sh
+++ typenex-medical-runner.sh
@@ -6,10 +6,14 @@ NC='\033[0m' # No Color
 # Default values for environment variables
 CAT="production"
 OXYGEN="production"
+GATSBY_ALGOLIA_INDEX="development"
+ALGOLIA_SKIP_INDEXING="true"
 
 # Flags to track if each option is set
 cat_local_flag=false
 oxygen_local_flag=false
+algolia_local_flag=false
+build_flag=false
 
 # Parse command line options
 while [[ $# -gt 0 ]]; do
@@ -20,6 +24,12 @@ while [[ $# -gt 0 ]]; do
         ;;
         -c|--cat)
         cat_local_flag=true
+        ;;
+        --algolia)
+        algolia_local_flag=true
+        ;;
+        --build)
+        build_flag=true
         ;;
         *)
         # unknown option
@@ -37,6 +47,11 @@ if [ "$oxygen_local_flag" = true ]; then
     OXYGEN="local"
 fi
 
+if [ "$algolia_local_flag" = true ]; then
+    GATSBY_ALGOLIA_INDEX="local"
+    ALGOLIA_SKIP_INDEXING="false"
+fi
+
 # Change directory to the emerging-artists-chicago project directory
 echo "Changing directory to ~/typenex-medical-gatsby..."
 cd ~/typenex-medical-gatsby
@@ -46,18 +61,25 @@ export CAT_DATA_LEVEL=staging
 echo -e "CAT_DATA_LEVEL is set to ${BLUE}$CAT_DATA_LEVEL${NC}"
 export HIERARCHY_LEVEL=development
 echo -e "HIERARCHY_LEVEL is set to ${BLUE}$HIERARCHY_LEVEL${NC}"
-export GATSBY_ALGOLIA_INDEX=development
+export GATSBY_ALGOLIA_INDEX="$GATSBY_ALGOLIA_INDEX"
 echo -e "GATSBY_ALGOLIA_INDEX is set to ${BLUE}$GATSBY_ALGOLIA_INDEX${NC}"
+export ALGOLIA_SKIP_INDEXING="$ALGOLIA_SKIP_INDEXING"
+echo -e "ALGOLIA_SKIP_INDEXING is set to ${BLUE}$ALGOLIA_SKIP_INDEXING${NC}"
 export CAT="$CAT"
 echo -e "CAT is set to ${BLUE}$CAT${NC}"
 export OXYGEN="$OXYGEN"
 echo -e "OXYGEN is set to ${BLUE}$OXYGEN${NC}"
-echo "Run with --cat or -c to set CAT to local or --oxygen or -o to set OXYGEN to local."
+echo "Run with --cat or -c to set CAT to local, --oxygen or -o to set OXYGEN to local, or --algolia to set GATSBY_ALGOLIA_INDEX to local and ALGOLIA_SKIP_INDEXING to false."
 
 # Start the local server
-echo -e "Running 'gatsby clean' and 'gatsby develop' in the typenex-medical-gatsby project directory...\n"
 gatsby clean
-gatsby develop
+if [ "$build_flag" = true ]; then
+    echo -e "RUNNING BUILD...\n"
+    gatsby build
+else
+    echo -e "Running 'gatsby clean' and 'gatsby develop' in the typenex-medical-gatsby project directory...\n"
+    gatsby develop
+fi
 
 # Print message indicating the script has finished
 echo "Run server process has finished."
